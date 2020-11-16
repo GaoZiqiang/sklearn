f4063111319ed2f28d537ee8a8ce7ea8ea585cf7
gaoziqiang <gaoziqianghi@163.com>
2020-11-16 22:32:29 +0800

    some changes

--- code_of_Python_Machine_Learning/chapter1_data_preprocessing/linear_regressor.py
+++ code_of_Python_Machine_Learning/chapter1_data_preprocessing/linear_regressor.py
@@ -66,3 +66,28 @@ with  open(output_model_file,'rb') as f:# 同样，读出用二进制读出'rb'
 # 使用模型
 y_test_pred_new = model_linregr.predict(X_test)
 print('new pred result:',sm.mean_squared_error(y_test,y_test_pred_new))
+
+# 多项式回归器
+from sklearn.preprocessing import PolynomialFeatures
+
+# 生成多项式，将曲线的多项式的次数的初始值设置为 3
+polynomial = PolynomialFeatures(degree=3)
+
+X_train_transformed = polynomial.fit_transform(X_train)# 用作训练数据
+
+# 下面做一组测试
+# datapoint = np.arange(6).reshape(6, 1)
+datapoint = [0.39,2.78,7.11]
+datapoint = np.reshape(datapoint,(3,1))
+# from IPython import embed
+# embed()
+poly_datapoint = polynomial.fit_transform(datapoint)# 对数据进行多项式处理
+
+# 实例化poly_linear_model
+poly_linear_model = linear_model.LinearRegression()
+# 训练模型
+poly_linear_model.fit(X_train_transformed,y_train)
+
+# 预测
+print('普通线性回归结果：',linear_regressor.predict(datapoint)[0])# 普通线性回归器
+print('多项式回归结果：',poly_linear_model.predict(poly_datapoint)[0])# 多项式回归器
